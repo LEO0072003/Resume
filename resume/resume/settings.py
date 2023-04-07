b69c71fb8abfedf4f36422f3d76c94488ea26662
LEO0072003 <106365961+LEO0072003@users.noreply.github.com>
2023-04-08 01:43:25 +0530

    Added sidebar

--- resume/resume/settings.py
+++ resume/resume/settings.py
@@ -153,10 +153,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+
 STATIC_URL = 'static/'
+
+STATIC_ROOT = 'static'
 
 
 MEDIA_ROOT = BASE_DIR / 'media'
