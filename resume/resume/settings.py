0d85333e49c87c0c55281bc108cd6f38ae2dffbc
LEO0072003 <106365961+LEO0072003@users.noreply.github.com>
2023-04-08 11:06:39 +0530

    edit Experience

--- resume/resume/settings.py
+++ resume/resume/settings.py
@@ -154,13 +154,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS=[
+    BASE_DIR / 'static'
+]
 
-STATIC_ROOT = 'static'
+STATIC_URL = '/static/'
+# STATIC_ROOT = 'static'
 
 
 MEDIA_ROOT = BASE_DIR / 'media'
-
 MEDIA_URL = 'media/'
 
 
